151ad102dd780b040242acbfaf09e0ea9ea2a332
lqdev <liquidev@tutanota.com>
2023-08-27 22:33:54 +0200

    split up client scripts

--- admin/client_edit.fish
+++ admin/client_edit.fish
@@ -6,16 +6,4 @@ echo
 set_color white --bold; echo "* Running editor"; set_color normal
 eval $EDITOR content/index.tree
 
-echo
-set_color white --bold; echo "* Fixing the tree"; set_color normal
-cargo run -p treehouse fix-all --apply
-
-echo
-set_color white --bold; echo "* Committing changes"; set_color normal
-git add content
-git commit
-git push
-
-echo
-set_color white --bold; echo "* Uploading to server"; set_color normal
-fish admin/client_sync.fish
+fish admin/client_push.fish
